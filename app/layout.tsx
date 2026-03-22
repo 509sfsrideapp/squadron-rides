a89e8d45156bb75358704baa219c1829ab4faaf7
509sfsrideapp <509sfsrideapp@gmail.com>
2026-03-21 22:20:19 -0500

    Remove Google font dependency for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Oxanium } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const oxanium = Oxanium({
-  variable: "--font-oxanium",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "509 SFS Rides",
@@ -28,10 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} ${oxanium.variable} h-full antialiased`}
-    >
+    <html lang="en" className="h-full antialiased">
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
